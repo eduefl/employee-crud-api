a87621fc6a879476efac2f263457853aeaadca29
Eduardo Ferreira Lima <elima@npl4m.ru>
2022-05-06 02:28:27 +0300

    setled routing of the 3 apps created

--- front-angular/src/app/app-routing.module.ts
+++ front-angular/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EmplyeeAddComponent } from './emplyee-add/emplyee-add.component';
+import { EmplyeeEditComponent } from './emplyee-edit/emplyee-edit.component';
+import { EmplyeeGetComponent } from './emplyee-get/emplyee-get.component';
+
+
+const routes: Routes = [
+  {path: "emplyee/add", component: EmplyeeAddComponent}
+  {path: "emplyee/:id", component: EmplyeeEditComponent}
+  {path: "emplyee", component: EmplyeeGetComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
