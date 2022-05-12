7eac88e9a80e09953a233692caa57af609fa4830
Eduardo Ferreira Lima <elima@npl4m.ru>
2022-05-13 02:46:57 +0300

    Implementation of SlimLoadingBar

--- front-angular/src/app/app.component.ts
+++ front-angular/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SlimLoadingBarService} from 'ng2-slim-loading-bar';
+import { NavigationCancel,Event, NavigationEnd, NavigationStart, NavigationError, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-angular';
+
+  /**
+   * method costructor responsible for set loading bar in Application
+   */
+  constructor(private loadingBar: SlimLoadingBarService, private router: Router) {
+     this.router.events.subscribe((event: Event)=> {
+       this.navigationInerceptorn(event);
+
+     });
+  }
+  /**
+   *
+   * @param event Methodo responsible for set the events for navigations used in loading bar
+   */
+  private navigationInerceptorn(event: Event): void{
+    if(event instanceof NavigationStart)
+    {
+      this.loadingBar.start();
+    }
+
+    if(event instanceof NavigationCancel)
+    {
+      this.loadingBar.stop();
+    }
+
+    if(event instanceof NavigationError)
+    {
+      this.loadingBar.stop();
+    }
+
+    if(event instanceof NavigationEnd)
+    {
+      this.loadingBar.complete();
+    }
+
+  }
 }
