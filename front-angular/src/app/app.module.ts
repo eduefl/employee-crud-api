b4f104c02f54e1030d337089985cdb5e723a5181
Eduardo Ferreira Lima <elima@npl4m.ru>
2022-06-25 17:40:30 +0300

    Included required validations in formBuilder

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {SlimLoadingBarModule} from 'ng2-slim-loading-bar';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { EmplyeeGetComponent } from './emplyee-get/emplyee-get.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SlimLoadingBarModule.forRoot()
+    SlimLoadingBarModule.forRoot(),
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-angular/src/app/emplyee-add/emplyee-add.component.ts
+++ front-angular/src/app/emplyee-add/emplyee-add.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-emplyee-add',
   templateUrl: './emplyee-add.component.html',
@@ -7,7 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmplyeeAddComponent implements OnInit {
 
-  constructor() { }
+  employeeForm !: FormGroup ;
+    constructor(private formBuilder: FormBuilder) {
+      this.createForm();
+     }
+
+     createForm(): void {
+      this.employeeForm = this.formBuilder.group({
+        employeeName:         ['', Validators.required],
+        jobRole:              ['', Validators.required],
+        salary:               ['', Validators.required],
+        dateBirth:            ['', Validators.required],
+        employeeRegistration: ['', Validators.required]
+      });
+     }
 
   ngOnInit(): void {
   }
